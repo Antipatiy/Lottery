6a4d0252ef15f0870a92ea2909c50f4b44c43878
Yury Krutko <yury.krutko@technavology.com>
2017-08-03 11:47:33 +0300

    wright ES8 style

--- app.js
+++ app.js
@@ -2,155 +2,304 @@
 
   "use strict";
 
-  var validator = {
+  let validator = {
     inputName: false,
     inputSurname: false,
     inputEmail: false
   };
 
-  var validatorRules = {
+  let validatorRules = {
     name: ['inputName', /^[a-zA-Z -]+$/, 'name'],
     surname: ['inputSurname', /^[a-zA-Z -]+$/, 'surname'],
     email: ['inputEmail', /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/, 'email']
   };
 
-  function Winner() {
-    var table = document.querySelector('#table');
-    var tableBody = table.querySelector('tbody');
-    var inputName = document.querySelector('#inputName');
-    var inputSurname = document.querySelector('#inputSurname');
-    var inputEmail = document.querySelector('#inputEmail');
-    var inputPhone = document.querySelector('#inputPhone');
-    var saveValues = document.querySelector('#save-values');
-    var alertText = document.querySelector('.alertText');
-    var winnerButton = document.querySelector('.winner__btn');
-    var newWinner = document.querySelector('.winner__new');
-    var arrOfPersons = [];
-    var that = this;
+  class Winner {
+    constructor() {
+      this.arrOfPersons = [];
+    }
 
-    that.isTrue = function (elem) {
+    getElem(selector) {
+      return document.querySelector(selector);
+    }
+
+    isTrue(elem) {
       return elem === true;
-    };
+    }
 
-    that.checkValidator = function () {
-      var booleanArr = [];
+    checkValidator() {
+      let booleanArr = [];
 
-      Object.keys(validator).forEach(function (key) {
+      Object.keys(validator).forEach((key) => {
         booleanArr.push(this[key]);
       }, validator);
 
-      return booleanArr.every(that.isTrue);
-    };
+      return booleanArr.every(this.isTrue);
+    }
 
-    that.validateOnBlur = function (elemName, regExp, field, context) {
+    validateOnBlur(elemName, regExp, field, context) {
       if (!context.value.toString().match(regExp)) {
-        alertText.textContent = 'Type correct ' + field;
+        this.getElem('.alertText').textContent = 'Type correct ' + field;
         context.focus();
       }
       else {
-        alertText.textContent = '';
+        this.getElem('.alertText').textContent = '';
         validator[elemName] = true;
       }
-    };
+    }
 
-    that.mountBlur = function (elem, elemName, regExp, field) {
+    mountBlur(elem, elemName, regExp, field) {
+      let that = this;
       elem.addEventListener('blur', function () {
         that.validateOnBlur(elemName, regExp, field, this);
       });
-    };
+    }
 
-    that.addNewPerson = function () {
-      arrOfPersons.push([
-        inputName.value.trim(),
-        inputSurname.value.trim(),
-        inputEmail.value.trim(),
-        inputPhone.value.trim()
+    addNewPerson() {
+      let that = this;
+      this.arrOfPersons.push([
+        that.getElem('#inputName').value.trim(),
+        that.getElem('#inputSurname').value.trim(),
+        that.getElem('#inputEmail').value.trim(),
+        that.getElem('#inputPhone').value.trim()
       ]);
-    };
+    }
 
-    that.setTableElems = function () {
-      var lastElem = arrOfPersons.length - 1;
-      var row = document.createElement("tr");
+    setTableElems() {
+      let lastElem = this.arrOfPersons.length - 1;
+      let row = document.createElement("tr");
 
-      for (var i = 0; i < 4; i++) {
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode(arrOfPersons[lastElem][i]);
+      for (let i = 0; i < 4; i++) {
+        let cell = document.createElement("td");
+        let cellText = document.createTextNode(this.arrOfPersons[lastElem][i]);
         cell.appendChild(cellText);
         row.appendChild(cell);
       }
-      tableBody.appendChild(row);
-    };
+      this.getElem('tbody').appendChild(row);
+    }
 
-    that.clearInputs = function () {
-      inputName.value = '';
-      inputSurname.value = '';
-      inputEmail.value = '';
-      inputPhone.value = '';
-    };
+    clearInputs() {
+      this.getElem('#inputName').value = '';
+      this.getElem('#inputSurname').value = '';
+      this.getElem('#inputEmail').value = '';
+      this.getElem('#inputPhone').value = '';
+    }
 
-    that.setFalse = function () {
-      for (var prop in validator) {
+    setFalse() {
+      for (let prop in validator) {
         validator[prop] = false;
       }
-    };
+    }
 
-    that.setTableRow = function (event) {
+    setTableRow(event) {
       event.preventDefault();
-      if (that.checkValidator()) {
-        that.addNewPerson();
-        that.setTableElems();
-        that.clearInputs();
-        that.setFalse();
+      if (this.checkValidator()) {
+        this.addNewPerson();
+        this.setTableElems();
+        this.clearInputs();
+        this.setFalse();
       }
       else {
-        alertText.textContent = 'Fill all inputs *';
+        this.getElem('.alertText').textContent = 'Fill all inputs *';
       }
-    };
+    }
 
-    that.mountClickOnSaveButton = function () {
-      saveValues.addEventListener('click', function (event) {
-        that.setTableRow(event);
+    mountClickOnSaveButton() {
+      this.getElem('#save-values').addEventListener('click', (event) => {
+        this.setTableRow(event);
       });
-    };
+    }
 
-    that.randomInteger = function (min, max) {
-      var rand = min + Math.random() * (max + 1 - min);
+    randomInteger(min, max) {
+      let rand = min + Math.random() * (max + 1 - min);
       rand = Math.floor(rand);
       return rand;
-    };
+    }
 
-    that.getArrMax = function () {
-      return arrOfPersons.length - 1;
-    };
+    getArrMax() {
+      return this.arrOfPersons.length - 1;
+    }
 
-    that.showWinner = function (text) {
-      newWinner.textContent = text;
-    };
+    showWinner(text) {
+      this.getElem('.winner__new').textContent = text;
+    }
 
-    that.getWinner = function () {
-      if (arrOfPersons.length === 0) {
+    getWinner() {
+      if (this.arrOfPersons.length === 0) {
         return;
       }
       else {
-        var newWinner = that.randomInteger(0, that.getArrMax());
-        that.showWinner(arrOfPersons[newWinner][0] + ' ' + arrOfPersons[newWinner][1]);
+        let newWinner = this.randomInteger(0, this.getArrMax());
+        this.showWinner(this.arrOfPersons[newWinner][0] + ' ' + this.arrOfPersons[newWinner][1]);
       }
-    };
+    }
 
-    that.mountClickOnWinnerButton = function () {
-      winnerButton.addEventListener('click', that.getWinner);
-    };
+    mountClickOnWinnerButton() {
+      this.getElem('.winner__btn').addEventListener('click', this.getWinner);
+    }
 
-    that.init = function () {
-      that.mountBlur(inputName, validatorRules.name[0], validatorRules.name[1], validatorRules.name[2]);
-      that.mountBlur(inputSurname, validatorRules.surname[0], validatorRules.surname[1], validatorRules.surname[2]);
-      that.mountBlur(inputEmail, validatorRules.email[0], validatorRules.email[1], validatorRules.email[2]);
-      that.mountClickOnSaveButton();
-      that.mountClickOnWinnerButton();
+    init() {
+      this.mountBlur(this.getElem('#inputName'), validatorRules.name[0], validatorRules.name[1], validatorRules.name[2]);
+      this.mountBlur(this.getElem('#inputSurname'), validatorRules.surname[0], validatorRules.surname[1], validatorRules.surname[2]);
+      this.mountBlur(this.getElem('#inputEmail'), validatorRules.email[0], validatorRules.email[1], validatorRules.email[2]);
+      this.mountClickOnSaveButton();
+      this.mountClickOnWinnerButton();
     }
   }
 
-  var winner = new Winner();
+  let winner = new Winner();
   winner.init();
 
 })();
+
+// (function () {
+//
+//   "use strict";
+//
+//   var validator = {
+//     inputName: false,
+//     inputSurname: false,
+//     inputEmail: false
+//   };
+//
+//   var validatorRules = {
+//     name: ['inputName', /^[a-zA-Z -]+$/, 'name'],
+//     surname: ['inputSurname', /^[a-zA-Z -]+$/, 'surname'],
+//     email: ['inputEmail', /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/, 'email']
+//   };
+//
+//   function Winner() {
+//
+//     var arrOfPersons = [];
+//     var that = this;
+//
+//     that.getElem = function (selector) {
+//       return document.querySelector(selector);
+//     };
+//
+//     that.isTrue = function (elem) {
+//       return elem === true;
+//     };
+//
+//     that.checkValidator = function () {
+//       var booleanArr = [];
+//
+//       Object.keys(validator).forEach(function (key) {
+//         booleanArr.push(this[key]);
+//       }, validator);
+//
+//       return booleanArr.every(that.isTrue);
+//     };
+//
+//     that.validateOnBlur = function (elemName, regExp, field, context) {
+//       if (!context.value.toString().match(regExp)) {
+//         that.getElem('.alertText').textContent = 'Type correct ' + field;
+//         context.focus();
+//       }
+//       else {
+//         that.getElem('.alertText').textContent = '';
+//         validator[elemName] = true;
+//       }
+//     };
+//
+//     that.mountBlur = function (elem, elemName, regExp, field) {
+//       elem.addEventListener('blur', function () {
+//         that.validateOnBlur(elemName, regExp, field, this);
+//       });
+//     };
+//
+//     that.addNewPerson = function () {
+//       arrOfPersons.push([
+//         that.getElem('#inputName').value.trim(),
+//         that.getElem('#inputSurname').value.trim(),
+//         that.getElem('#inputEmail').value.trim(),
+//         that.getElem('#inputPhone').value.trim()
+//       ]);
+//     };
+//
+//     that.setTableElems = function () {
+//       var lastElem = arrOfPersons.length - 1;
+//       var row = document.createElement("tr");
+//
+//       for (var i = 0; i < 4; i++) {
+//         var cell = document.createElement("td");
+//         var cellText = document.createTextNode(arrOfPersons[lastElem][i]);
+//         cell.appendChild(cellText);
+//         row.appendChild(cell);
+//       }
+//       that.getElem('tbody').appendChild(row);
+//     };
+//
+//     that.clearInputs = function () {
+//       that.getElem('#inputName').value = '';
+//       that.getElem('#inputSurname').value = '';
+//       that.getElem('#inputEmail').value = '';
+//       that.getElem('#inputPhone').value = '';
+//     };
+//
+//     that.setFalse = function () {
+//       for (var prop in validator) {
+//         validator[prop] = false;
+//       }
+//     };
+//
+//     that.setTableRow = function (event) {
+//       event.preventDefault();
+//       if (that.checkValidator()) {
+//         that.addNewPerson();
+//         that.setTableElems();
+//         that.clearInputs();
+//         that.setFalse();
+//       }
+//       else {
+//         that.getElem('.alertText').textContent = 'Fill all inputs *';
+//       }
+//     };
+//
+//     that.mountClickOnSaveButton = function () {
+//       that.getElem('#save-values').addEventListener('click', function (event) {
+//         that.setTableRow(event);
+//       });
+//     };
+//
+//     that.randomInteger = function (min, max) {
+//       var rand = min + Math.random() * (max + 1 - min);
+//       rand = Math.floor(rand);
+//       return rand;
+//     };
+//
+//     that.getArrMax = function () {
+//       return arrOfPersons.length - 1;
+//     };
+//
+//     that.showWinner = function (text) {
+//       that.getElem('.winner__new').textContent = text;
+//     };
+//
+//     that.getWinner = function () {
+//       if (arrOfPersons.length === 0) {
+//         return;
+//       }
+//       else {
+//         var newWinner = that.randomInteger(0, that.getArrMax());
+//         that.showWinner(arrOfPersons[newWinner][0] + ' ' + arrOfPersons[newWinner][1]);
+//       }
+//     };
+//
+//     that.mountClickOnWinnerButton = function () {
+//       that.getElem('.winner__btn').addEventListener('click', that.getWinner);
+//     };
+//
+//     that.init = function () {
+//       that.mountBlur(that.getElem('#inputName'), validatorRules.name[0], validatorRules.name[1], validatorRules.name[2]);
+//       that.mountBlur(that.getElem('#inputSurname'), validatorRules.surname[0], validatorRules.surname[1], validatorRules.surname[2]);
+//       that.mountBlur(that.getElem('#inputEmail'), validatorRules.email[0], validatorRules.email[1], validatorRules.email[2]);
+//       that.mountClickOnSaveButton();
+//       that.mountClickOnWinnerButton();
+//     }
+//   }
+//
+//   var winner = new Winner();
+//   winner.init();
+//
+// })();
